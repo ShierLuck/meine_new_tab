d36b3712c94374c4ee705b6d0ff77b7ba9b2ebeb
ShierLuck <shinliang39@gmail.com>
2020-07-01 19:11:49 +0700

    Hatsu

---
+++ start/search/shigoto.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+    
+    var d = new Date();
+    var year = d.getFullYear();
+    var month = d.getMonth()+1;
+    month = (month<10 ? '0' : '') + month;
+
+    $('.sort_date').val(year+'-'+month);
+    var param = { 
+        is_ajax: true, 
+        month: $('.sort_date').val()
+    };
+    setShigoto(param);
+
+    $('.btn-store-shigoto').on('click', function(e){
+        e.preventDefault();
+        var data = { is_ajax: true, };
+        var url = 'http://djun.indonesiafintechforum.org/meine_note/public/api/store_shigoto';
+        $.ajax({
+            url: url,
+            type: "post",
+            data: data
+        }).done(function (result) {
+          var param = { is_ajax: true, };
+          setShigoto(param);
+        }).fail();
+    });
+});
+
+$('.btn-remove-shigto').on('click', function(e){
+    e.preventDefault();
+    var ini = $(this), id = ini.data('id');
+    console.log(id);
+    var data = { is_ajax: true, };
+    var url = 'http://djun.indonesiafintechforum.org/meine_note/public/api/remove_shigoto/'+id;
+    $.ajax({
+        url: url,
+        type: "post",
+        data: data
+    }).done(function (result) {
+      setShigoto();
+    }).fail();
+});
+
+$('.sort_date').on('change', function(e){
+    e.preventDefault();
+    var ini = $(this), month = ini.val();
+    
+    var param = { 
+        is_ajax: true, 
+        month: month
+    };
+    setShigoto(param);
+    
+});
+
+function setShigoto(data) {
+    var t_body = $('.shigoto-tbody');
+    var url = 'http://djun.indonesiafintechforum.org/meine_note/public/api/getShigoto';
+    $.ajax({
+        url: url,
+        type: "post",
+        data: data
+    }).done(function (result) {
+      if(result.status){
+        datas = result.data;
+        var naka = "";
+        $.each(datas, function( index, value ) {
+            naka += "<tr>"+
+                        "<td>"+
+                            datas[index].id +
+                        "</td>"+
+                        "<td>"+
+                            datas[index].check_in +
+                        "</td>"+
+                        "<td>"+
+                            datas[index].check_out +
+                        "</td>"+
+                        "<td>"+
+                            "<button class='btn btn-remove-shigto' data-id='" +
+                                datas[index].id +
+                            "'>DEL</button>" +
+                        "</td>"+
+                    "</tr>";
+        });
+        t_body.html(naka);
+      }
+    }).fail();
+}
